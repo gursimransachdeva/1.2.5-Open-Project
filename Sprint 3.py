4562e85a4de9abd6bbbc51442f5ff120b9423cfd
gursimransachdeva <sachdeva8150@mydusd.org>
2025-11-18 09:55:51 -0800

    Update Sprint 3.py

--- Sprint 3.py
+++ Sprint 3.py
@@ -13,6 +13,7 @@ Dark_Green = (80, 200, 120)
 Pixel_Size = 50
 Clock = pygame.time.Clock() 
 SNAKE_SPEED = 10 
+Game_Over = False
 
 Snake_Body = [(0, 0)]
 Snake_Direction = "RIGHT"
@@ -37,8 +38,8 @@ def generate_new_apple_position():
         y = rand.choice(Grid_Coords)
         if (x, y) not in Snake_Body:
             return x, y
-
 Random_Apple_X, Random_Apple_Y = generate_new_apple_position()
+
 def Drawing_The_Grid():
     for y in range(0, Screen_Height, Pixel_Size):
         for x in range(0, Screen_Width, Pixel_Size):
@@ -53,6 +54,20 @@ def Draw_Snake():
             screen.blit(Player_Image, (segment_x, segment_y))
         else:
             screen.blit(Snake_Body_Image, (segment_x, segment_y))
+
+def Snake_Head_To_Body_Collision():
+    head_position = Snake_Body[0]
+    body_segments = Snake_Body[1:] 
+    if head_position in body_segments:
+        return True
+    return False
+
+def Check_Wall_Collision():
+    """Checks if the snake's head goes outside the screen boundaries."""
+    head_x, head_y = Snake_Body[0]
+    if head_x < 0 or head_x >= Screen_Width or head_y < 0 or head_y >= Screen_Height:
+        return True
+    return False
 
 def Draw_Apple():
     screen.blit(Game_Display_Image, (Random_Apple_X, Random_Apple_Y))
@@ -90,6 +105,7 @@ def handle_collectable(ate_apple):
     if ate_apple:
         Player_Score = Player_Score + 1
         Random_Apple_X, Random_Apple_Y = generate_new_apple_position()
+        print(Player_Score)
 
 running = True
 frame_count = 0
@@ -98,8 +114,7 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
-        
-        if event.type == pygame.KEYDOWN:
+        if not Game_Over and event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and Snake_Direction != "DOWN":
                 Next_Direction = "UP"
             elif event.key == pygame.K_DOWN and Snake_Direction != "UP": 
@@ -107,15 +122,19 @@ while running:
             elif event.key == pygame.K_RIGHT and Snake_Direction != "LEFT":
                 Next_Direction = "RIGHT"
             elif event.key == pygame.K_LEFT and Snake_Direction != "RIGHT":
-                Next_Direction = "LEFT" 
+                Next_Direction = "LEFT"
     
-    frame_count = frame_count + 1
+    if not Game_Over:
+        frame_count = frame_count + 1
+
     if frame_count % SNAKE_SPEED == 0:
         ate_apple = move_snake()
         handle_collectable(ate_apple)
+        if Snake_Head_To_Body_Collision() or Check_Wall_Collision():
+                Game_Over = True
     Drawing_The_Grid()
     Draw_Apple()
-    Draw_Snake()  
+    Draw_Snake()
     pygame.display.flip()
     Clock.tick(60)
 
